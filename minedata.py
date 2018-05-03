72c2c65e7f19522aa7a5ca1cd63a154928e06664
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2018-05-03 15:00:45 +0200

    Implement the % cancelled flights per day query

--- minedata.py
+++ minedata.py
@@ -1,20 +1,27 @@
 
+from datetime import *
 import pyspark as sk
+import pyspark.sql.types as skt
 import functools as ft
 
 
-def get_data(sc: sk.sql.SparkSession, test: bool) -> sk.sql.DataFrame:
+def get_data(spark: sk.sql.SparkSession, test: bool) -> sk.sql.DataFrame:
     rootdir = 'data' if not test else 'test_data'
     ext = '.csv.bz2' if not test else '.csv'
-    sets = [sc.read.csv(rootdir+'/'+str(year)+ext) for year in range(1994, 2008-1)]
+    sets = [spark.read.csv(rootdir+'/'+str(year)+ext, header=True) for year in range(1994, 2008+1)]
     return ft.reduce(lambda x, y: x.union(y), sets)
+
+
+def perc_cancelled_flights_per_day(spark: sk.sql.SparkSession, data: sk.sql.DataFrame) -> sk.sql.DataFrame:
+    info = data.rdd.map(lambda row: (date(int(row['Year']), int(row['Month']), int(row['DayofMonth'])), int(row['Cancelled'])))
+    fractioncancelled = info.foldByKey((0, 0), lambda l, r: (l[0]+1, l[1]+r))
+    return fractioncancelled.mapValues(lambda v: v[1]/v[0]*100.0).sortByKey()
 
 
 if __name__ == '__main__':
     spark = sk.sql.SparkSession.builder.master("local").appName("mw spark bigdata").getOrCreate()
     data = get_data(spark, True)
-
-    print(data.count())
+    print(perc_cancelled_flights_per_day(spark, data).take(10))
 
 
 
