9b37091eb251114a38db7a83098b8ae267c90404
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2018-05-03 15:42:26 +0200

    Implement the weekly weather cancellation %age query.

--- minedata.py
+++ minedata.py
@@ -13,15 +13,26 @@ def get_data(spark: sk.sql.SparkSession, test: bool) -> sk.sql.DataFrame:
 
 
 def perc_cancelled_flights_per_day(spark: sk.sql.SparkSession, data: sk.sql.DataFrame) -> sk.sql.DataFrame:
-    info = data.rdd.map(lambda row: (date(int(row['Year']), int(row['Month']), int(row['DayofMonth'])), int(row['Cancelled'])))
+    info = data.rdd.map(
+        lambda row: (date(int(row['Year']), int(row['Month']), int(row['DayofMonth'])), int(row['Cancelled'])))
     fractioncancelled = info.foldByKey((0, 0), lambda l, r: (l[0]+1, l[1]+r))
     return fractioncancelled.mapValues(lambda v: v[1]/v[0]*100.0).sortByKey()
+
+
+def perc_weather_cancellations_per_week(spark: sk.sql.SparkSession, data: sk.sql.DataFrame) -> sk.sql.DataFrame:
+    onlycancelled = data.select(data['Cancelled'] == 1)
+    codeperweek = data.rdd.map(
+        lambda row: (
+            date(int(row['Year']), int(row['Month']), int(row['DayofMonth'])) - timedelta(int(row['DayOfWeek'])-1),
+            row['CancellationCode'].strip()))
+    fractioncancelled = codeperweek.foldByKey((0, 0), lambda l, r: (l[0] + 1, l[1] + (1 if r == 'B' else 0)))
+    return fractioncancelled.mapValues(lambda v: v[1] / v[0] * 100.0).sortByKey()
 
 
 if __name__ == '__main__':
     spark = sk.sql.SparkSession.builder.master("local").appName("mw spark bigdata").getOrCreate()
     data = get_data(spark, True)
     print(perc_cancelled_flights_per_day(spark, data).take(10))
-
+    print(perc_weather_cancellations_per_week(spark, data).take(10))
 
 
