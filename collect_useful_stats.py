69acd95cca38097865d2244a3a2294b30e18ae66
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2018-06-20 18:32:26 +0200

    Fix NA distances for valid flights.

--- collect_useful_stats.py
+++ collect_useful_stats.py
@@ -33,7 +33,6 @@ def max_and_min_distance(spark: sk.sql.SparkSession, data: sk.sql.DataFrame):
 def distances_per_path(spark: sk.sql.SparkSession, data: sk.sql.DataFrame):
     def process_row(row):
         try:
-            dist = float(row['Distance'])
             origin = row['Origin'].strip()
             dest = row['Dest'].strip()
             if origin > dest:
@@ -42,7 +41,13 @@ def distances_per_path(spark: sk.sql.SparkSession, data: sk.sql.DataFrame):
                 dest = tmp
         except:
             return []
-        return [(origin+'-'+dest, {dist})]
+        pathkey = origin+'-'+dest
+
+        try:
+            dist = float(row['Distance'])
+        except:
+            return [(pathkey, {-999.0})]
+        return [(pathkey, {dist})]
 
     def setunion(v1:set, v2:set):
         return v1.union(v2)
@@ -53,6 +58,18 @@ def distances_per_path(spark: sk.sql.SparkSession, data: sk.sql.DataFrame):
     return numdists
 
 
+def no_distance_and_not_cancelled(spark: sk.sql.SparkSession, data: sk.sql.DataFrame):
+    def process_row(row):
+        cancelled = 1
+        try:
+            cancelled = int(row['Cancelled'])
+            dist = float(row['Distance'])
+        except:
+            return (cancelled == 0)
+        return False
+    return data.rdd.filter(process_row)
+
+
 if __name__ == '__main__':
     test = True
     if test:
@@ -61,11 +78,19 @@ if __name__ == '__main__':
         addr = "spark://remote:7077"
 
     spark = sk.sql.SparkSession.builder.master(addr).appName("mw spark bigdata tool").getOrCreate()
+
     data = get_data(spark, test)
+
     max_and_min_distance(spark, data)
+
     res = distances_per_path(spark, data)
     onlymultiple = res.filter(lambda kv: kv[1][0] > 1)
+    onlywithvoid = res.filter(lambda kv: -999.0 in kv[1][1])
     print(onlymultiple.take(10))
+    print(onlywithvoid.take(10))
+
+    ndanc = no_distance_and_not_cancelled(spark, data)
+    print(ndanc.take(10))
 
 
 
--- minedata.py
+++ minedata.py
@@ -35,18 +35,60 @@ def perc_weather_cancellations_per_week(spark: sk.sql.SparkSession, data: sk.sql
     return fractioncancelled.mapValues(lambda v: v[1] / v[0] * 100.0).sortByKey()
 
 
-def perc_dep_delay_halved_per_group(spark: sk.sql.SparkSession, data: sk.sql.DataFrame) -> sk.RDD:
+def path_key(row: sk.sql.Row) -> str:
+    origin = row['Origin'].strip()
+    dest = row['Dest'].strip()
+    if origin > dest:
+        tmp = origin
+        origin = dest
+        dest = tmp
+    return origin+'-'+dest
+
+
+def distances_per_path(spark: sk.sql.SparkSession, data: sk.sql.DataFrame) -> sk.RDD:
     def process_row(row):
         try:
-            distgroup = max(1, int(row['Distance']) // 200)
-            arrdelay = float(row['ArrDelay'].strip())
-            depdelay = float(row['DepDelay'].strip())
+            dist = int(row['Distance'])
+            pathkey = path_key(row)
         except:
             return []
+        return [(pathkey, dist)]
+
+    def setunion(v1:set, v2:set):
+        return v1.union(v2)
+
+    distperpath = data.rdd.flatMap(process_row)
+    validdists = distperpath.reduceByKey(lambda v1, v2: v1 if v1 == v2 else -999)
+    dists = validdists.filter(lambda kv: kv[1] >= 0)
+    return dists
+
+
+def perc_dep_delay_halved_per_group(spark: sk.sql.SparkSession, data: sk.sql.DataFrame) -> sk.RDD:
+    dists = distances_per_path(spark, data)
+    dist_and_delay_per_path = data.rdd.map(lambda row: (path_key(row), (row['Distance'], row['ArrDelay'], row['DepDelay'])))
+    dist_and_delays_per_path = dist_and_delay_per_path.leftOuterJoin(dists)
+
+    def process(kv):
+        val = kv[1]
+        fixed_distance = val[1]
+        dist_arrdelay_depdelay = val[0]
+        if not (fixed_distance is None):
+            distance = fixed_distance
+        else:
+            try:
+                distance = int(dist_arrdelay_depdelay[0])
+            except:
+                return []
+        try:
+            arrdelay = float(dist_arrdelay_depdelay[1].strip())
+            depdelay = float(dist_arrdelay_depdelay[2].strip())
+        except:
+            return []
+        distgroup = max(1, distance // 200)
         delayfrac = (1, 1 if (arrdelay <= (depdelay * 0.5)) else 0)
         return [(distgroup, delayfrac)]
 
-    delayhalvedpergroup = data.rdd.flatMap(process_row)
+    delayhalvedpergroup = dist_and_delays_per_path.flatMap(process)
     fracdelayhalved = delayhalvedpergroup.reduceByKey(lambda l, r: (l[0]+r[0], l[1]+r[1]))
     return fracdelayhalved.mapValues(lambda v: v[1] / v[0] * 100.0).sortByKey()
 
@@ -76,14 +118,19 @@ if __name__ == '__main__':
 
     spark = sk.sql.SparkSession.builder.master("local").appName("mw spark bigdata").getOrCreate()
     data = get_data(spark, True)
+
+    pddhpg = perc_dep_delay_halved_per_group(spark, data)
+    print(pddhpg.take(10))
+    exit(0)
+
     cfpd = perc_cancelled_flights_per_day(spark, data)
     pwcpw = perc_weather_cancellations_per_week(spark, data)
-    pddhpg = perc_dep_delay_halved_per_group(spark, data)
+
     ppa = penalty_per_airport(spark, data)
 
     print(cfpd.take(10))
     print(pwcpw.take(10))
-    print(pddhpg.take(10))
+
     print(ppa.take(10))
 
     if len(sys.argv) > 1:
