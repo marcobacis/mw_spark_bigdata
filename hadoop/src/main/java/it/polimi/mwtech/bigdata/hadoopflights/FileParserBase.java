d3811a35c64b3457c550acfd1c5ade52d8e00d62
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2018-07-09 22:39:47 +0200

    Ignore the header line

--- hadoop/src/main/java/it/polimi/mwtech/bigdata/hadoopflights/FileParserBase.java
+++ hadoop/src/main/java/it/polimi/mwtech/bigdata/hadoopflights/FileParserBase.java
@@ -8,7 +8,7 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 
 
-abstract public class FileParserBase<KEYOUT, VALUEOUT> extends Mapper<Object, Text, KEYOUT, VALUEOUT>
+abstract public class FileParserBase<KEYOUT, VALUEOUT> extends Mapper<LongWritable, Text, KEYOUT, VALUEOUT>
 {
   /* Declaration order must match column order in data files */
   public enum RowKey {
@@ -42,28 +42,24 @@ abstract public class FileParserBase<KEYOUT, VALUEOUT> extends Mapper<Object, Te
     SEC_DELAY_MINS,
     LATE_AIRCRAFT_DELAY_MINS
   };
-  
-  
-  public void map(Object key, Text value, Context context) throws IOException, InterruptedException
+
+
+  public void map(LongWritable linenum, Text value, Context context) throws IOException, InterruptedException
   {
+    if (linenum.get() == 0)
+      return;
+
     String sval = value.toString();
     String rawvals[] = sval.split(",");
     RowKey keys[] = RowKey.values();
-    
+
     HashMap<RowKey, String> row = new HashMap<RowKey, String>();
     int count = Integer.min(rawvals.length, keys.length);
-    
-    for (int i=0; i<count; i++) {
+
+    for (int i = 0; i < count; i++) {
       row.put(keys[i], rawvals[i].trim());
     }
-    
-    String testyear = row.get(RowKey.YEAR);
-    try {
-      Integer.parseInt(testyear);
-    } catch (NumberFormatException e) {
-      return;
-    }
-    
+
     KEYOUT keyout = selectKey(row);
     if (keyout != null) {
       VALUEOUT valout = selectValue(row);
@@ -75,8 +71,8 @@ abstract public class FileParserBase<KEYOUT, VALUEOUT> extends Mapper<Object, Te
 
 
   abstract public KEYOUT selectKey(Map<RowKey, String> row);
-  
-  
+
+
   abstract public VALUEOUT selectValue(Map<RowKey, String> row);
 
 }
